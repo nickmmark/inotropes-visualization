72ebd58b803261e6a4a1d0d7dc45e1c60b71c73b
Nick Mark, MD <48685552+nickmmark@users.noreply.github.com>
2024-11-03 10:21:17 -0800

    Add files via upload
    added angiotensin II

--- inotropes_heatmap.R
+++ inotropes_heatmap.R
@@ -6,10 +6,10 @@ library(ggplot2)
 
 # Define data - modified from several sources. See www.criticalcaretime.com episode on inotropes for more
 data <- data.frame(
-  x = c(2, 2.1, 2, 2.2, 1.5, 1, 0.2, 0.5, 1.4), # inotropic effect
-  y = c(1.5, 1.8, 2.2, 2.5, 0, 0.5, 0.3, 0.8, 0.2), # chronotropic effect
-  z = c(-1, 2, 1, -1, -2, 2, -1, 1, -1.8), # vascular effect (negative = vasodilator, positive = vasoconstrictor)
-  labels = c("Dobutamine", "Epinephrine (high dose)", "Dopamine (high dose)", "Isoproterenol", "Milrinone", "Norepinephrine", "Dopamine (low dose)", "Epinephrine (low dose)", "Levosimendan")
+  x = c(2, 2.1, 2, 2.2, 1.5, 1, 0.2, 0.5, 1.4, 0.5), # inotropic effect
+  y = c(1.5, 1.8, 2.2, 2.5, 0, 0.5, 0.3, 0.8, 0.2, 0.7), # chronotropic effect
+  z = c(-1.2, 2, 1.8, -0.5, -2, 2, -1, 1, -1.8, 2), # vascular effect (negative = vasodilator, positive = vasoconstrictor)
+  labels = c("Dobutamine", "Epinephrine (high dose)", "Dopamine (high dose)", "Isoproterenol", "Milrinone", "Norepinephrine", "Dopamine (low dose)", "Epinephrine (low dose)", "Levosimendan", "Angiotensin II")
 )
 
 # Create XY plot with heatmap colors based on Z axis
@@ -25,8 +25,8 @@ ggplot(data, aes(x = x, y = y)) +
     x = "Inotropic Effect",
     y = "Chronotropic Effect"
   ) +
-  coord_cartesian(xlim = c(min(data$x), max(data$x) + 1.4), 
-                  ylim = c(min(data$y), max(data$y) + 0.2)) + # Further expand space for labels
+  coord_cartesian(xlim = c(min(data$x), max(data$x) + 0.8), 
+                  ylim = c(min(data$y), max(data$y) + 0.0 )) + # Further expand space for labels
   theme_minimal() +
   theme(
     axis.text.x = element_blank(),  # Remove X-axis numbers
